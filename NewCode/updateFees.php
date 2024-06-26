6ec20c6f6953cc1769d655b9a752467bb8a13036
whisper150 <shahame1@southernct.edu>
2024-06-26 19:56:30 -0400

    Update updateFees.php
    Finished first version of script; hopefully this works.
    - Also, changed the names of tables and attributes to be inline with the actual DB

--- NewCode/updateFees.php
+++ NewCode/updateFees.php
@@ -8,14 +8,14 @@
     // When using this script, the patronID of the patron requesting this service should be sent in using POST
     $patronid = $_POST["patronid"];
     // Retrieve the current fines that the patron owes
-    $stmt = $conn->prepare("SELECT late_fees_owed FROM patrons WHERE patronid=?");
+    $stmt = $conn->prepare("SELECT Late_Fees_Owed FROM Patrons WHERE Patron_Id=?");
     $stmt->bind_param("i", $patronid);
     $stmt->execute();
     $current_fines = $stmt->get_result();
     
     
     // Retrieve all CHECK OUT DATES (from which due dates are derived) for all books in 'checked_out' where the due date is smaller than the current date:
-    $stmt = $conn->prepare("SELECT checked_out FROM books WHERE ISBN=(SELECT ISBN FROM checkouts WHERE patronid=? AND ( checkout_date + 30 < CURRENT_DATE() )");
+    $stmt = $conn->prepare("SELECT Checked_Out FROM Books WHERE ISBN=(SELECT ISBN FROM Checkouts WHERE Patron_Id=? AND ( Checkout_Date + 30 < CURRENT_DATE() )");
     $stmt->bind_param("i", $patronid);
     $stmt->execute();
     $late_book_dates = $stmt->get_result();
@@ -23,18 +23,15 @@
     // Add up all the owed late fees
     $added_fees = 0.0
     foreach ($late_book_dates as $time) {
-        $late_period = strtotime($time) + strtotime(30);
-        $added_fees = $added_fees + ($time * 0.5)
+        $late_period = strtotime("now") - (strtotime($time) + strtotime(30));
+        $added_fees = $added_fees + ($late_period * 0.5);
     }
+
+    $stmt  = $conn->prepare("UPDATE Patrons SET Owed_Late_Fees = (Patrons.Owed_Late_Fees + ?)");
+    $stmt->bind_param("i", $added_fees);\
+    $stmt->execute();
     
     
     $conn->close();
 ?>
 
-
-
-/*$stmt = $conn->prepare("SELECT id, firstname, lastname FROM MyGuests");
-
-$stmt->execute();
-$result = $stmt->get_result();
-*/
