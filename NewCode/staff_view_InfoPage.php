40ef5bfa37aeee40d23e6e952b57f0b7fb39c51c
whisper150 <shahame1@southernct.edu>
2024-06-26 14:50:05 -0400

    Update and rename staff_view_InfoPage to staff_view_InfoPage.php
    Created a basic form for library staff to use to request information about all book loans or all book holds

---
+++ NewCode/staff_view_InfoPage.php
@@ -0,0 +1,22 @@
+<html>
+<body>
+
+    <div style="border: 5px solid black; padding: 10px">
+        <form action="staff_show_book_info.php" method="post">
+
+          <p>Enter type of info desired ("loans" or "holds")</p>
+          <p>Lowercase only.</p>
+        	<span> 
+        		Enter info type: <input type="text" name="infoType" style="margin: 8px 2px">
+        		<br>
+        	</span>
+        	
+        	<span> 
+        		<input type="submit" style="margin: 8px 2px">
+        	</span>
+        </form>
+    </div>
+
+</body>
+</html>
+
