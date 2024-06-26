18b330bae0404057553e43e7e8478c8f2cf48715
whisper150 <shahame1@southernct.edu>
2024-06-25 23:18:55 -0400

    Create patron_registration.php

---
+++ NewCode/patron_registration.php
@@ -0,0 +1,23 @@
+<html>
+<body>
+    
+    <form action="add_patrons.php" method="post">
+    	
+    	<span style = 'position:fixed; left: 30%;; top: 30%;'> 
+    		First Name: <input type="text" name="user_name">
+    		<br>
+    	</span>
+    	
+    	<span style = 'position:fixed; left: 30%;; top: 40%;'> 
+    		Last Name: <input type="text" name="email"><br>
+    	</span>
+    	
+    	<input type="password" name = "pass_field">
+    	
+    	<span style = 'position:fixed; left: 30%;; top: 50%;'> 
+    		<input type="submit">
+    	</span>
+    </form>
+
+</body>
+</html>
