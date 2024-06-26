e96e7dcbb2ad87aa893c5d357c69300f8d601739
whisper150 <shahame1@southernct.edu>
2024-06-25 23:22:42 -0400

    Update patron_registration.php

--- NewCode/patron_registration.php
+++ NewCode/patron_registration.php
@@ -1,20 +1,29 @@
 <html>
 <body>
     
-    <form action="add_patrons.php" method="post">
+    <form action="add_patrons.php" method="post" style="border: 5px solid black">
     	
     	<span style = 'position:fixed; left: 30%;; top: 30%;'> 
-    		First Name: <input type="text" name="user_name">
+    		First Name: <input type="text" name="fname">
     		<br>
     	</span>
     	
     	<span style = 'position:fixed; left: 30%;; top: 40%;'> 
-    		Last Name: <input type="text" name="email"><br>
+    		Last Name: <input type="text" name="lname">
+            <br>
     	</span>
     	
-    	<input type="password" name = "pass_field">
-    	
     	<span style = 'position:fixed; left: 30%;; top: 50%;'> 
+    		Address: <input type="text" name="address">
+            <br>
+    	</span>
+
+        <span style = 'position:fixed; left: 30%;; top: 60%;'> 
+    		Date of Birth: <input type="text" name="dob">
+            <br>
+    	</span>
+    	
+    	<span style = 'position:fixed; left: 30%;; top: 70%;'> 
     		<input type="submit">
     	</span>
     </form>
