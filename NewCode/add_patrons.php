bd82987342498e1a57343d88b0ec5ad60abaf44b
whisper150 <shahame1@southernct.edu>
2024-06-25 23:48:42 -0400

    Update add_patrons.php
    Continued the 'insert into table' query, and almost finished it; must still figure out a feasible way to create unique ID numbers for each new patron

--- NewCode/add_patrons.php
+++ NewCode/add_patrons.php
@@ -10,9 +10,15 @@
 	
 	$conn = get_db_connection($db_name);
 	
-	// sql to create table
+	/*
+		=======================
+  		Must still think of a way to give each new patron a unique ID number
+    		=======================
+	*/
+	
+	// Insert new values into the table:
 	$sql = "INSERT INTO patrons (patronid, fname, lname, date_of_birth, address, fees, loaned_books)
-	VALUES (  )";
+	VALUES ($_POST["fname"], $_POST["lname"], $_POST["dob"], $_POST["address"], 0.0, 0)";
 	
 	try {
 	    $conn->query($sql);
