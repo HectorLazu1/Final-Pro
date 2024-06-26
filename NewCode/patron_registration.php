879700a2b4a1fea820a9812bf4137421c7effbda
whisper150 <shahame1@southernct.edu>
2024-06-25 23:23:51 -0400

    Update patron_registration.php

--- NewCode/patron_registration.php
+++ NewCode/patron_registration.php
@@ -1,32 +1,34 @@
 <html>
 <body>
-    
-    <form action="add_patrons.php" method="post" style="border: 5px solid black">
-    	
-    	<span style = 'position:fixed; left: 30%;; top: 30%;'> 
-    		First Name: <input type="text" name="fname">
-    		<br>
-    	</span>
-    	
-    	<span style = 'position:fixed; left: 30%;; top: 40%;'> 
-    		Last Name: <input type="text" name="lname">
-            <br>
-    	</span>
-    	
-    	<span style = 'position:fixed; left: 30%;; top: 50%;'> 
-    		Address: <input type="text" name="address">
-            <br>
-    	</span>
 
-        <span style = 'position:fixed; left: 30%;; top: 60%;'> 
-    		Date of Birth: <input type="text" name="dob">
-            <br>
-    	</span>
-    	
-    	<span style = 'position:fixed; left: 30%;; top: 70%;'> 
-    		<input type="submit">
-    	</span>
-    </form>
+    <span style="border: 5px solid black">
+        <form action="add_patrons.php" method="post">
+        	
+        	<span style = 'position:fixed; left: 30%;; top: 30%;'> 
+        		First Name: <input type="text" name="fname">
+        		<br>
+        	</span>
+        	
+        	<span style = 'position:fixed; left: 30%;; top: 40%;'> 
+        		Last Name: <input type="text" name="lname">
+                <br>
+        	</span>
+        	
+        	<span style = 'position:fixed; left: 30%;; top: 50%;'> 
+        		Address: <input type="text" name="address">
+                <br>
+        	</span>
+    
+            <span style = 'position:fixed; left: 30%;; top: 60%;'> 
+        		Date of Birth: <input type="text" name="dob">
+                <br>
+        	</span>
+        	
+        	<span style = 'position:fixed; left: 30%;; top: 70%;'> 
+        		<input type="submit">
+        	</span>
+        </form>
+    </span>
 
 </body>
 </html>
