50a7e8f6a2a53c9899e984d59ba0052d3bac5c75
whisper150 <shahame1@southernct.edu>
2024-06-26 22:16:28 -0400

    Update staff_view_InfoPage.php
    Added a new form to allow for adding new entries to the Books table

--- NewCode/staff_view_InfoPage.php
+++ NewCode/staff_view_InfoPage.php
@@ -31,7 +31,51 @@
         	</span>
         </form>
     </div>
-    
+
+    <div style="border: 5px solid black; padding: 10px; margin: 10px 0px">
+        <form action="staff_add_book.php" method="post">
+
+          <h2>Add books to catalog</h2>
+        	<span> 
+        		Enter ISBN of new book: <input type="text" name="addISBN" style="margin: 8px 2px">
+        		<br>
+        	</span>
+
+            <span> 
+        		Enter title of new book: <input type="text" name="add_title" style="margin: 8px 2px">
+        		<br>
+        	</span>
+
+            <span> 
+        		Enter author of new book: <input type="text" name="add_author" style="margin: 8px 2px">
+        		<br>
+        	</span>
+
+            <span> 
+        		Enter edition of new book: <input type="text" name="add_edition" style="margin: 8px 2px">
+        		<br>
+        	</span>
+
+            <span> 
+        		Enter publisher of new book: <input type="text" name="add_publisher" style="margin: 8px 2px">
+        		<br>
+        	</span>
+
+            <span> 
+        		Enter publication year of new book: <input type="text" name="add_year" style="margin: 8px 2px">
+        		<br>
+        	</span>
+
+            <span> 
+        		Enter call number of new book: <input type="text" name="add_call_number" style="margin: 8px 2px">
+        		<br>
+        	</span>
+        	
+        	<span> 
+        		<input type="submit" style="margin: 8px 2px">
+        	</span>
+        </form>
+    </div>
 
 </body>
 </html>
