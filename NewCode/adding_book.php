9c1b52409d78432239d0eca89a68e8ccaa09f04b
whisper150 <shahame1@southernct.edu>
2024-06-27 17:52:24 -0400

    Update adding_book.php

--- NewCode/adding_book.php
+++ NewCode/adding_book.php
@@ -1,45 +1,29 @@
-<form method="post" action="adding_book.php">
-    ISBN: <input type="text" name="isbn"><br>
-    Book Title: <input type="text" name="title"><br>
-    Book Author: <input type="text" name="author"><br>
-    Publisher: <input type="text" name="publisher"><br>
-    Publication Year: <input type="text" name="year"><br>
-    Book Edition: <input type="text" name="edition"><br>
-    Call Number: <input type="text" name="call_number"><br>
-    Checkout Status: <input type="text" name="checkout_status"><br>
-    Renewals: <input type="text" name="renewals"><br>
-    Late Fee: <input type="text" name="late_fee"><br>
-    Loaning Patron ID: <input type="text" name="loaning_patron_id"><br>
-    <input type="submit" value="Add Book">
-</form>
-
 
 <?php
 
-ini_set('display_errors', 1);
-ini_set('display_startup_errors', 1);
-error_reporting(E_ALL);
+	// Establish connection with the database:
+	include '../connect/connect_to_db.php';
+	$db_name = 'library_try';	
+	$conn = get_db_connection($db_name);
 
-include 'db_connect_simple.php';
+	/*ini_set('display_errors', 1);
+	ini_set('display_startup_errors', 1);
+	error_reporting(E_ALL);
 
-if ($_SERVER["REQUEST_METHOD"] == "POST") {
-    $isbn = $_POST["isbn"];
-    $title = $_POST["title"];
-    $author = $_POST["author"];
-    $publisher = $_POST["publisher"];
-    $year = $_POST["year"];
-    $edition = $_POST["edition"];
-    $call_number = $_POST["call_number"];
-    $checkout_status = $_POST["checkout_status"];
-    $renewals = $_POST["renewals"];
-    $late_fee = $_POST["late_fee"];
-    $loaning_patron_id = $_POST["loaning_patron_id"];
+	include 'db_connect_simple.php';*/
 
-    
+	if ($_SERVER["REQUEST_METHOD"] == "POST") {
+		$isbn = $_POST["addISBN"];
+		$title = $_POST["add_title"];
+		$author = $_POST["add_author"];
+		$publisher = $_POST["add_publisher"];
+		$year = $_POST["add_year"];
+		$edition = $_POST["add_edition"];
+		$call_number = $_POST["add_call_number"];    
 
 
-    $stmt = $conn->prepare("INSERT INTO Books (ISBN, Book_Title, Book_Author, Publisher, Publication_Year, Book_Edition, Call_Number, Checkout_Status, Renewals, Late_Fee, Loaning_Patron_ID) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
-    $stmt->bind_param("ssssisssidi", $isbn, $title, $author, $publisher, $year, $edition, $call_number, $checkout_status, $renewals, $late_fee, $loaning_patron_id);
+    $stmt = $conn->prepare("INSERT INTO Books (ISBN, Book_Title, Book_Author, Publisher, Publication_Year, Book_Edition, Call_Number) VALUES (?, ?, ?, ?, ?, ?, ?)");
+    $stmt->bind_param("ssssiss", $isbn, $title, $author, $publisher, $year, $edition, $call_number);
 
     // Execute the statement
     if ($stmt->execute()) {
