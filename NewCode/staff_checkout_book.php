f9862ce7dc73009c82ed6749d3a57ceafa693ddc
whisper150 <shahame1@southernct.edu>
2024-06-26 23:58:31 -0400

    Update staff_checkout_book.php
    Updated the script to check if a book is already checked out to someone else before checking it out to the requesting patron

--- NewCode/staff_checkout_book.php
+++ NewCode/staff_checkout_book.php
@@ -4,10 +4,20 @@
 	$db_name = 'test_db';	
 	$conn = get_db_connection($db_name);
 
-	// Checkout a book:
-	$stmt = $conn->prepare("INSERT INTO Checkouts (PatronID, ISBN, Checkout_Date, Renewals_Left) VALUES (?, ?, ?, ?)");
-    $stmt->bind_param("iisi", $_POST["checkout_id"], $_POST["checkout_ISBN"], CURRENT_DATE(), 3);
+	// Check if the requested book is already checked out:
+	$stmt = $conn->prepare("SELECT EXISTS (SELECT * FROM Checkouts WHERE ISBN=?)");
+    $stmt->bind_param("i", $_POST["checkout_ISBN"]);
     $stmt->execute();
+	$exists_bool = $stmt->get_result();
+
+	if ($exists_bool == 0){
+		// If the book is not already checked out, then checkout the book:
+		$stmt = $conn->prepare("INSERT INTO Checkouts (PatronID, ISBN, Checkout_Date, Renewals_Left) VALUES (?, ?, ?, ?)");
+    	$stmt->bind_param("iisi", $_POST["checkout_id"], $_POST["checkout_ISBN"], CURRENT_DATE(), 3);
+    	$stmt->execute();
+	} else{
+		echo "Unable to checkout book; This book is already checked out to another patron."
+	}
 	
 	
 	$conn->close();
