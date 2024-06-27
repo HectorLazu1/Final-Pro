8aa9bf906e60bc36d6fc30c87524ff3b7a23c9d7
whisper150 <shahame1@southernct.edu>
2024-06-26 21:35:12 -0400

    Update staff_view_InfoPage.php
    Added a form to be used for deleting books

--- NewCode/staff_view_InfoPage.php
+++ NewCode/staff_view_InfoPage.php
@@ -4,8 +4,8 @@
     <div style="border: 5px solid black; padding: 10px">
         <form action="staff_show_book_info.php" method="post">
 
-          <p>Enter type of info desired ("loans" or "holds")</p>
-          <p>Lowercase only.</p>
+          <h2>Enter type of info desired ("loans" or "holds").</h2>
+          <h3>Lowercase only.</h3>
         	<span> 
         		Enter info type: <input type="text" name="infoType" style="margin: 8px 2px">
         		<br>
@@ -16,6 +16,22 @@
         	</span>
         </form>
     </div>
+
+    <div style="border: 5px solid black; padding: 10px; margin: 10px 0px">
+        <form action="staff_delete_book.php" method="post">
+
+          <h2>Remove books from catalog</h2>
+        	<span> 
+        		Enter ISBN: <input type="text" name="deleteISBN" style="margin: 8px 2px">
+        		<br>
+        	</span>
+        	
+        	<span> 
+        		<input type="submit" style="margin: 8px 2px">
+        	</span>
+        </form>
+    </div>
+    
 
 </body>
 </html>
