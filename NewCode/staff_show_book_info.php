5e37c3a2f4e9d0d29dc55797c0acc5f1b5da536a
rini3636 <117469046+rini3636@users.noreply.github.com>
2024-06-27 13:03:17 -0400

    Update staff_show_book_info.php

--- NewCode/staff_show_book_info.php
+++ NewCode/staff_show_book_info.php
@@ -10,12 +10,28 @@
         $stmt = $conn->prepare("SELECT * FROM Loans");
         $stmt->execute();
         $result = $stmt->get_result();
-        echo $result
+	    // Output the results
+	if ($result->num_rows > 0) {
+		while ($row = $result->fetch_assoc()) {
+			echo "ID: " . $row["PatronID"] . " - Book: " . $row["ISBN"] ." - Checkout Date: ". $row["Checkout_Date"] . " - Renewals Left: " . $row["Renewals_Left"] . "<br>";
+		}
+		else {
+		echo "No records found.";
+        //Line not used, but scared to delete
+	//echo $result;
     } elseif ($_POST["infoType"] == "holds") {
         $stmt = $conn->prepare("SELECT * FROM Holds");
         $stmt->execute();
         $result = $stmt->get_result();
-        echo $result
+        // Output the results
+		if ($result->num_rows > 0) {
+			while ($row = $result->fetch_assoc()) {
+				echo "ID: " . $row["PatronID"] . " - Book: " . $row["ISBN"] . "<br>";
+    }
+		} else {
+			echo "No records found.";
+	//Line not used, but scared to delete
+	//echo $result;
     }
     
     $conn->close();
