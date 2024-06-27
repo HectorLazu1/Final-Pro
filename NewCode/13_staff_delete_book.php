196c43b38ab2cba2609d38fa0b8e45877f1265c1
whisper150 <shahame1@southernct.edu>
2024-06-26 21:45:08 -0400

    Update and rename 13_staff_delete_book to 13_staff_delete_book.php
    - Renamed file to .php
    - Finished first version of script

---
+++ NewCode/13_staff_delete_book.php
@@ -0,0 +1,13 @@
+<?php
+	// Establish connection with the database:
+	include '../connect/connect_to_db.php';
+	$db_name = 'test_db';	
+	$conn = get_db_connection($db_name);
+
+	$stmt = $conn->prepare("DELETE Books WHERE ISBN = ?");
+    $stmt->bind_param("i", $_POST["deleteISBN"]);
+    $stmt->execute();
+	
+	
+	$conn->close();
+?>
